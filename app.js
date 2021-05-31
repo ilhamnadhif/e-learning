1face109eb2900175da336a385eff8fc9b6480eb
Ilham Nadhif <ilham.10rpl2.2019@gmail.com>
2021-05-31 20:07:22 +0700

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-require("dotenv").config()
+require("dotenv").config();
+
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
